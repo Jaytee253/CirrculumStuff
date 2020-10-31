851366bf9d22c5eb678102d10810621d8d72a91c
Jaytee253 <jaytee253@icloud.com>
2020-10-30 20:16:57 -0700

    switch demo here. chnages coming

--- src/Demo1.java
+++ src/Demo1.java
@@ -1,7 +1,18 @@
 public class Demo1 {
 
     public static void main(String[] args){
-
+            int num1 = 69;
+            switch (num1) {
+                case 68:
+                    System.out.println("yes");
+                    break;
+                case 69:
+                    System.out.println("eat");
+                    break;
+                default:
+                    System.out.println("pp");
+                    break;
+            }
         }
 
     }
